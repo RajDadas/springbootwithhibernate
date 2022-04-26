cea11b9f150270ecae11e45f288b95cfbcc8c34b
RajDadas <raj.dadas@gmail.com>
2022-04-26 18:27:54 +0530

    initial commit for spring boot app

---
+++ SpringBootWithHibernate/SpringBootWithHibernate/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,23 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v2")
+public class ApplicationController {
+
+    @GetMapping ("/savedata")
+    public String saveData(){
+
+        return " Data Saved Sucessfully";
+    }
+    @GetMapping("/getdata")
+   public String getData(){
+        return " Getting Data";
+    }
+
+
+}
